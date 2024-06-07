cbb25d6fee1c9a436ca27999a1cee81e8ddb0d8e
tamireshc <tamireshc@gmail.com>
2024-06-07 09:18:40 -0300

    feat: add configurations at next.config.mjs for images, clean initial config project and add global styles

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  imagens: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'dogsapi.origamid.dev'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
