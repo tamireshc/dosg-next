5fce1093ab5fd88aca62d9dfdd470e58d41e36fd
tamireshc <tamireshc@gmail.com>
2024-06-20 09:37:48 -0300

    feat: implement userStats

--- src/app/conta/estatisticas/page.tsx
+++ src/app/conta/estatisticas/page.tsx
@@ -1,13 +1,26 @@
+import statsGet from "@/actions/stats-get";
 import { Metadata } from "next";
+import dynamic from "next/dynamic";
+
+//lasy loading
+const ContaEstatisticas = dynamic(
+  () => import("@/components/conta/conta-estatisticas"),
+  {
+    loading: () => <p>carregando</p>,
+    ssr: false, // somente carega no cliente ao abrir a página, não há carregamento no servidor antecipado
+  }
+);
 
 export const metadata: Metadata = {
   title: "Postar | Estatísticas",
 };
 
-export default function EstatisticasPage() {
+export default async function EstatisticasPage() {
+  const data = await statsGet();
+  if (!data?.data) return null;
   return (
-    <main>
-      <h1>Estatisticas</h1>
-    </main>
+    <section>
+      <ContaEstatisticas data={data.data} />
+    </section>
   );
 }
