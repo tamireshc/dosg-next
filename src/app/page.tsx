43aa441a7559d8e4ea9341e9d8d23c5c80b36c54
tamireshc <tamireshc@gmail.com>
2024-06-20 11:12:31 -0300

    fix: return of data null

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Feed from "@/components/feed/feed";
 
 export default async function Home() {
   const data = await photosGet();
+  if (!data?.data) return null;
   return (
     <section className="container mainContainer">
       <Feed photos={data?.data} />
